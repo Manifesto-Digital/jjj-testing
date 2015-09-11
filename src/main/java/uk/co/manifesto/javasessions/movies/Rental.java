b33d531b30053610ebf2387adf869a50f36c8ca3
Patrick Shone <patrick.shone@vodafone.com>
2015-09-11 11:45:18 +0100

    first commit

---
+++ src/main/java/uk/co/manifesto/javasessions/movies/Rental.java
@@ -0,0 +1,20 @@
+package uk.co.manifesto.javasessions.movies;
+
+class Rental {
+	
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+}
